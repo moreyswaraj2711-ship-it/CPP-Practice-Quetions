06b88c286bd34de85a442d1fb0fe03a29974a259
moreyswaraj2711-ship-it <moreyswaraj2711@gmail.com>
2025-11-27 15:17:22 +0530

    Feat: Forloop program created

---
+++ starprogram.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for(int i=0;i<4;i++)
+    {
+        // cout<<" * ";
+        for(int x=0;x<3;x++)
+        {
+            cout<<" * ";
+        }
+        // cout<<" * ";
+        // cout<<" \n";
+    }
+      return 0;
+}
