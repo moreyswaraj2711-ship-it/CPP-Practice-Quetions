5d78a52028aa93923d667145bb9129957cc038f9
moreyswaraj2711-ship-it <moreyswaraj2711@gmail.com>
2025-12-01 15:23:13 +0530

    Feat: Volume of sphere is created

---
+++ Area of Circle.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float  r;
+    cout<<"enter the radius: ";
+    cin>>r;
+    cout<<3.14*r*r;
+
+}
---
+++ volume of sphere.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float r;
+    cout<<"Enter the radius :";
+    cin>>r;
+    cout<<"Volume of spere is: \n " ; 
+    cout<< (4*3.14*r*r*r)/3;
+}
