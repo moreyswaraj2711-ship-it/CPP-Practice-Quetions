5884faa513b20fd4f67ca4af03d3182770c0a2b2
moreyswaraj2711-ship-it <moreyswaraj2711@gmail.com>
2025-12-18 11:16:20 +0530

    Feat: Even And Odd Number program created

---
+++ practice.cpp/even odd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<< "Enter the no: ";
+    cin>> x;
+    if(x%2==0)
+    {
+        cout<<"Even Number";
+    }
+    else
+    {
+        cout<<"Odd Number";
+    }
+}
