194118f699e1fa47167b005b25aee317ee5ffc3d
moreyswaraj2711-ship-it <moreyswaraj2711@gmail.com>
2025-12-18 11:27:19 +0530

    Feat: Practice problem of conditional statement is created

---
+++ practice.cpp/divisible by 5.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int x;
+     cout<< " enter the no:";
+     cin>>x;
+     if(x%5==0)
+     {
+        cout<<" Number is divisible by 5";
+     }
+     else{
+        cout<<"Number is not divisle by 5";
+     }
+}
