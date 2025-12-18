603542e20b921fcffcfb4a9d95ff56c9bb55470d
moreyswaraj2711-ship-it <moreyswaraj2711@gmail.com>
2025-12-18 19:03:13 +0530

    Feat: Comparison between area and perimeter of rectangle

---
+++ practice.cpp/area and perimeter.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"Enter the length:";
+    cin>>x;
+    int y;
+    cout<<"Enter the width:";
+    cin>>y;
+    if()
+}
