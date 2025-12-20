30d16dbcefb37c5b04aba636e89d1cd8fb433595
moreyswaraj2711-ship-it <moreyswaraj2711@gmail.com>
2025-12-20 21:08:23 +0530

    Feat: Switch case prgram created

---
+++ practice.cpp/switchcase.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin>> x;
+    char op;
+    cin>> op;
+    int y;
+    cin>> y;
+    switch(op){
+        case '+':
+        cout<<x+y<<endl;
+        break;
+         case '-':
+        cout<<x-y<<endl;
+        break;
+         case '*':
+        cout<<x*y<<endl;
+        break;
+         case '/':
+        cout<<x/y<<endl;
+        break;
+        default:
+        cout<<"Invalid oprerator";
+        break;
+    }}
