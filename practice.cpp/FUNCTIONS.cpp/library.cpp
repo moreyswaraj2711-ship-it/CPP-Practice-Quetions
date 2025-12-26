edb6fce95b01998a43d0b49c420a535d759f73cb
moreyswaraj2711-ship-it <moreyswaraj2711@gmail.com>
2025-12-27 00:31:26 +0530

    Feat: Implement Library functions in C++

---
+++ practice.cpp/FUNCTIONS.cpp/library.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    cout<<cbrt(10);
+    cout<<min(17,9);
+    cout<<max(67,90);
+    cout<<sqrt(8);
+    cout<<pow(2,8);
+}
