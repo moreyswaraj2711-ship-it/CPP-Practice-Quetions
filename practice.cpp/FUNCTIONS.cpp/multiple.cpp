88a58296d2b2aa36353f300c3fabd0dc272e2848
moreyswaraj2711-ship-it <moreyswaraj2711@gmail.com>
2025-12-26 23:42:01 +0530

    Feat : Implement creation of  functions in C++

---
+++ practice.cpp/FUNCTIONS.cpp/multiple.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void usa()
+{
+    cout<<" You are In USA ";
+}
+void india(){
+    cout<<" You are In INDIA ";
+}
+int main()
+{
+    usa();
+    cout<<" Your are In Main";
+    india();
+}
---
+++ practice.cpp/PATTERN.cpp/serialNotriangle.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<< " Enter the rows :";
+    cin >> n;
+    int a=1;
+     for(int i=1;i<=n;i++)
+     {
+       
+        for(int j=1;j<=i;j++)
+        {
+            cout<<a<< " ";
+            a=a+1;
+        }
+        cout<<endl;
+     }
+     return 0;
+}
