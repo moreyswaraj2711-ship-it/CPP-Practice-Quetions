00c36550a24df0c78235e08208a7fd63b0d487ba
moreyswaraj2711-ship-it <moreyswaraj2711@gmail.com>
2026-01-02 13:16:58 +0530

    Feat: Implemented swapping two using method in c++

---
+++ practice.cpp/FUNCTIONS.cpp/swaptwono1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<" Enter the no :";
+    cin>>x;
+    cout<<" ENter the no ;";
+    cin>>y;
+    x=x+y;
+    y=x-y;
+    x=x-y;
+    cout<<"After swapping"<<x<<endl <<y<<endl;
+}
